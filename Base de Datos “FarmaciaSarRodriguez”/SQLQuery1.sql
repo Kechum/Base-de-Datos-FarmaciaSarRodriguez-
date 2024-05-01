c643252deb68da3c157c18732fa3bc836251e17b
Maria Fittipaldi <Kechum12@gmail.com>
2024-05-01 17:57:28 -0300

    Update
    Creacion Nueva Tabla Empleados.
    Values:
    • ID_Empleado (entero, clave primaria)
    • Nombre (texto)
    • Apellido (texto)
    • Cargo (texto)
    • Salario (decimal)

--- Base de Datos “FarmaciaSarRodriguez”/SQLQuery1.sql
+++ Base de Datos “FarmaciaSarRodriguez”/SQLQuery1.sql
@@ -11,3 +11,14 @@ Direccion Varchar (100),
 Telefono Varchar (50),
 CONSTRAINT PK_Clientes PRIMARY KEY  (ID_Cliente)
 );
+
+Create Table Empleados
+(
+  ID_Empleado int not null,
+  Nombre varchar(50),
+  Apellido varchar(50),
+  Cargo varchar(30),
+  Salario decimal(15,2)
+
+  Constraint PK_Empleado PRIMARY KEY (ID_Empleado)
+);
