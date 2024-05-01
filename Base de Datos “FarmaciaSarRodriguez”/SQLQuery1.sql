cc590f5cc589a8b399708200a2d823ac5f4b3e7a
JennyEchenique <jenneche91@gmail.com>
2024-05-01 14:43:36 -0300

    Creacion Tabla Clientes

--- Base de Datos “FarmaciaSarRodriguez”/SQLQuery1.sql
+++ Base de Datos “FarmaciaSarRodriguez”/SQLQuery1.sql
@@ -2,3 +2,12 @@ Create DataBase FarmaciaSarRodriguez
 
 use FarmaciaSarRodriguez
 Go
+
+create TABLE clientes (
+ID_Cliente int not null,
+Nombre Varchar (50),
+Apellido Varchar (50),
+Direccion Varchar (100),
+Telefono Varchar (50),
+CONSTRAINT PK_Clientes PRIMARY KEY  (ID_Cliente)
+);
