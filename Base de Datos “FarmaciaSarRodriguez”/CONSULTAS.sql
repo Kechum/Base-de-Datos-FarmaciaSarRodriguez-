f66ded6a25c8e0c7a0785f26454238c9f778944a
JennyEchenique <jenneche91@gmail.com>
2024-05-09 18:02:12 -0300

    CONSULTAS

---
+++ Base de Datos “FarmaciaSarRodriguez”/CONSULTAS.sql
@@ -0,0 +1,8 @@
+-- 1) Nombre y dirección de todos los clientes.
+SELECT Nombre, Apellido, Direccion FROM clientes;
+
+-- 2) Nombre y cargo de todos los empleados.
+SELECT Nombre, Apellido, Cargo FROM Empleados;
+
+-- 3) Nombre y precio de todos los productos.
+SELECT Nombre, Precio FROM Productos;
